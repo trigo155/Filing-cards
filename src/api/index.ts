aa152cbd3fcf2c051ed71d624ad7f01c2641e9d2
Juan José Trigo Hormigos <trigo.hormigos@bootcamp-upgrade.com>
2021-12-06 22:54:58 +0100

    Feat create api folder and index.ts to call api

---
+++ src/api/index.ts
@@ -0,0 +1,14 @@
+import { Icoment } from "../types";
+
+export const getData = async (): Promise<Array<Icoment>> => {
+  let auxData: Icoment[] = [];
+  const api = "https://jsonplaceholder.typicode.com/posts/";
+  try {
+    const response = await fetch(api);
+    auxData = await response.json();
+    return auxData;
+  } catch (error) {
+    console.log("Error", error);
+  }
+  return auxData;
+};
