b5bfe028373381a025f1df852a3fd8fd43e9b967
Juan José Trigo Hormigos <trigo.hormigos@bootcamp-upgrade.com>
2021-12-15 00:20:58 +0100

    Feat create Edit component

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import Card from "./Card/Card";
 import PostCards from "./PostsCards/PostCards";
 import Navbar from "./Navbar/Navbar"
+import Edit from "./Edit/Edit";
 
-export { Card, PostCards, Navbar };
+export { Card, PostCards, Navbar, Edit };
